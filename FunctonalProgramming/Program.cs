1f5d646ecd1537c3fe94b3e468ac9f24eb2af151
STAR COMPUTER SYSTEM <prajjwaldhakal92@gmail.com>
2024-06-17 19:35:26 +0545

    Function

---
+++ FunctonalProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctonalProgramming
+{
+    class PureFunctionExample
+    {
+        static int Add(int x, int y) => x + y;
+
+        static void Main()
+        {
+            int result = Add(3, 5);
+            Console.WriteLine($"Result of Add(3, 5): {result}");
+        }
+    }
+}
---
+++ HigherOrderFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherOrderFunction
+{
+    class HigherOrderFunctionExample
+    {
+        static void Main()
+        {
+
+            Func<int, int, int> operation = Add; /*Func<int, int, int> operation = (x, y) => x + y;*/
+            int result = ApplyOperation(operation, 3, 5);
+            Console.WriteLine($"Result of applying Add(3, 5): {result}"); 
+        }
+        static int ApplyOperation(Func<int, int, int> operation, int x, int y)
+        {
+            return operation(x, y);
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
